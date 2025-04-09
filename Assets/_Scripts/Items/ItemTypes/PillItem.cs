0c887a01cfef7d4ffaace2a63692d26e3368fd21
goatfishplays <goatfishplays@gmail.com>
2025-04-09 13:06:57 -0700

    tolerance system
    TODO: Change the aoe heal thing button, it overlaps with sprint, either that or make sprint happen automatically after moving for x time

--- Assets/_Scripts/Items/ItemTypes/PillItem.cs
+++ Assets/_Scripts/Items/ItemTypes/PillItem.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ItemSO", menuName = "Items/Pill")]
 public class PillItem : ItemSO
 {
-    public int healAmount;
+    public float healAmount;
+    public float toleranceAmount;
 
-    public PillItem(string itemName, string description = "no desc", bool consumeOnUse = true, Sprite sprite = null, int healAmount = 5) : base(itemName, description, consumeOnUse, sprite)
+    public PillItem(string itemName, string description = "no desc", bool consumeOnUse = true, Sprite sprite = null, float healAmount = 5, float toleranceAmount = 1f) : base(itemName, description, consumeOnUse, sprite)
     {
         this.healAmount = healAmount;
+        this.toleranceAmount = toleranceAmount;
     }
 
     public override bool AttemptUse(Entity user)
     {
         Debug.Log($"{user.gameObject.name} used {name} to change health by {healAmount}");
-        user.entityHealth.ChangeHealth(healAmount, 0f, true);
+        // user.entityHealth.ChangeHealth(healAmount, 0f, true);
+        user.GetComponent<ToleranceManager>().Heal(healAmount, toleranceAmount);
         return true;
     }
 }
