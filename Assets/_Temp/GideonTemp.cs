84b0fb83f2ed1cc5a39634c96085a7af20a1f40f
Zippybyte <62920339+Zippybyte@users.noreply.github.com>
2025-04-10 22:34:40 -0700

    Add basic player attack

--- Assets/_Temp/GideonTemp.cs
+++ Assets/_Temp/GideonTemp.cs
@@ -4,14 +4,24 @@ using UnityEngine.InputSystem;
 public class Gideon_Temp : MonoBehaviour
 {
     public Entity player;
-    private InputAction damageAction;
     public PlayerInput playerInput;
+    public Transform projectileSpawnPoint;
+    public GameObject projectilePrefab;
+    public Camera mainCam;
+    public float projectileSpeed = 4f;
+
+    private InputAction damageAction;
+    private InputAction shootAction;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         damageAction = playerInput.actions.FindAction("TempDamage");
         damageAction.performed += TakeDamage;
+
+        shootAction = playerInput.actions.FindAction("TempShoot");
+        shootAction.performed += Shoot;
     }
 
     // Update is called once per frame
@@ -23,5 +33,18 @@ public class Gideon_Temp : MonoBehaviour
     void TakeDamage(InputAction.CallbackContext context)
     {
         player.entityHealth.ChangeHealth(-5f);
+    }
+
+    void Shoot(InputAction.CallbackContext context)
+    {
+        // Creates a projectile and puts it at the player and with the same position and rotation.
+        GameObject p = Instantiate(projectilePrefab, projectileSpawnPoint.position, projectileSpawnPoint.rotation);
+        
+        Rigidbody rb = p.GetComponent<Rigidbody>();
+        
+        rb.linearVelocity = mainCam.transform.forward * projectileSpeed;
+        p.GetComponent<Attack>().ownerId = player.id;
+
+        Debug.Log(mainCam.transform.forward);
     }
 }
