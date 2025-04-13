bd3afcc0a7f5eee48e781f63e26e9e7a9a374989
Zippybyte <62920339+Zippybyte@users.noreply.github.com>
2025-04-12 21:42:48 -0700

    Add projectile speed to ProjectileSpawner script

--- Assets/_Scripts/Attacks/ProjectileSpawner.cs
+++ Assets/_Scripts/Attacks/ProjectileSpawner.cs
@@ -6,14 +6,16 @@ public class ProjectileSpawner : MonoBehaviour
     public Transform projectileSpawnPoint;
     public Transform viewDirection;
     public GameObject projectilePrefab;
-    void Shoot()
+    public float projectileSpeed = 10f; // TODO: Move this into the weapon's properties
+
+    public void Shoot()
     {
         // Creates a projectile and puts it at the player and with the same position and rotation.
-        GameObject p = Instantiate(projectilePrefab, projectileSpawnPoint.position, projectileSpawnPoint.rotation);
+        GameObject projectile = Instantiate(projectilePrefab, projectileSpawnPoint.position, projectileSpawnPoint.rotation);
 
-        Rigidbody rb = p.GetComponent<Rigidbody>();
+        Rigidbody rb = projectile.GetComponent<Rigidbody>();
 
-        rb.linearVelocity = viewDirection.forward;
-        p.GetComponent<Attack>().ownerId = owner.id;
+        rb.linearVelocity = viewDirection.forward * projectileSpeed;
+        projectile.GetComponent<Attack>().ownerId = owner.id;
     }
 }
