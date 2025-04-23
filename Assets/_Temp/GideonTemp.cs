5f8bbd9ca630c09293d15fd3964773d9bcd0887b
Zippybyte <62920339+Zippybyte@users.noreply.github.com>
2025-04-22 22:14:15 -0700

    Remove temp shoot action

--- Assets/_Temp/GideonTemp.cs
+++ Assets/_Temp/GideonTemp.cs
@@ -5,13 +5,9 @@ public class Gideon_Temp : MonoBehaviour
 {
     public Entity player;
     public PlayerInput playerInput;
-    public Transform projectileSpawnPoint;
-    public GameObject projectilePrefab;
     public Camera mainCam;
-    public float projectileSpeed = 4f;
 
     private InputAction damageAction;
-    private InputAction shootAction;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -20,8 +16,6 @@ public class Gideon_Temp : MonoBehaviour
         damageAction = playerInput.actions.FindAction("TempDamage");
         damageAction.performed += TakeDamage;
 
-        shootAction = playerInput.actions.FindAction("TempShoot");
-        shootAction.performed += Shoot;
     }
 
     // Update is called once per frame
@@ -33,18 +27,5 @@ public class Gideon_Temp : MonoBehaviour
     void TakeDamage(InputAction.CallbackContext context)
     {
         player.entityHealth.ChangeHealth(-5f);
-    }
-
-    void Shoot(InputAction.CallbackContext context)
-    {
-        // Creates a projectile and puts it at the player and with the same position and rotation.
-        GameObject p = Instantiate(projectilePrefab, projectileSpawnPoint.position, projectileSpawnPoint.rotation);
-        
-        Rigidbody rb = p.GetComponent<Rigidbody>();
-        
-        rb.linearVelocity = mainCam.transform.forward * projectileSpeed;
-        p.GetComponent<Attack>().ownerId = player.id;
-
-        Debug.Log(mainCam.transform.forward);
     }
 }
