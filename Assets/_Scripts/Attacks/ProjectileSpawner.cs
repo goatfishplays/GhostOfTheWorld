f1527bb03859d9021e73133d16fe9e137dd4aa31
Zippybyte <62920339+Zippybyte@users.noreply.github.com>
2025-06-04 14:39:09 -0700

    Make projectile spawner instantiates on the correct parent

--- Assets/_Scripts/Attacks/ProjectileSpawner.cs
+++ Assets/_Scripts/Attacks/ProjectileSpawner.cs
@@ -6,12 +6,14 @@ public class ProjectileSpawner : MonoBehaviour
     public Transform projectileSpawnPoint;
     public Transform viewDirection;
     public GameObject projectilePrefab;
+    [Tooltip("Usually --- Attacks ---")]
+    public Transform parent;
     public float projectileSpeed = 10f; // TODO: Move this into the weapon's properties
 
     public void Shoot()
     {
         // Creates a projectile and puts it at the player and with the same position and rotation.
-        GameObject projectile = Instantiate(projectilePrefab, projectileSpawnPoint.position, projectileSpawnPoint.rotation);
+        GameObject projectile = Instantiate(projectilePrefab, projectileSpawnPoint.position, projectileSpawnPoint.rotation, parent);
 
         Rigidbody rb = projectile.GetComponent<Rigidbody>();
 
