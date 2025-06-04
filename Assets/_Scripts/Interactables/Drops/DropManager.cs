04adfc91e623ad55dc735fb1ffee12fa21f7949b
goatfishplays <goatfishplays@gmail.com>
2025-06-04 13:03:17 -0700

    UI Prefab + some comments added

--- Assets/_Scripts/Interactables/Drops/DropManager.cs
+++ Assets/_Scripts/Interactables/Drops/DropManager.cs
@@ -19,13 +19,29 @@ public class DropManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
-
+    /// <summary>
+    /// Creates a drop from the information contained in the itemSO
+    /// </summary>
+    /// <param name="pos">Position to spawn drop</param>
+    /// <param name="rot">Rotation of drop</param>
+    /// <param name="item">ItemSO containing the prefab to instantiate</param>
+    /// <param name="count">Amount of the item upon pickup</param>
+    /// <param name="lockTime">Amount of time before the item can be picked up(mainly useful if interactOnContact is set true in InteractableSO)</param>
     public void CreateDrop(Vector3 pos, Quaternion rot, ItemSO item, int count = 1, float lockTime = 0.1f)
     {
         GameObject spawned = Instantiate(item.dropPrefab, pos, rot, holder);
         spawned.GetComponentInChildren<Drop>().Initailize(item, count, lockTime);
     }
 
+    /// <summary>
+    /// Creates a ectoplasm drop from the information contained in the itemSO
+    /// Allows instantInteract function to work
+    /// </summary>
+    /// <param name="pos">Position to spawn drop</param>
+    /// <param name="rot">Rotation of drop</param> 
+    /// <param name="item">ItemSO containing the prefab to instantiate</param>
+    /// <param name="count">Amount of the item upon pickup</param>
+    /// <param name="lockTime">Amount of time before the item can be picked up(mainly useful if interactOnContact is set true in InteractableSO)</param>
     public void CreateEctoplasmDrop(Vector3 pos, Quaternion rot, ItemSO item, int count = 1, float lockTime = 0.1f)
     {
         GameObject spawned = Instantiate(item.dropPrefab, pos, rot, holder);
