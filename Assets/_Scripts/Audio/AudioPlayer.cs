a00c5fe9f5c085d7468587ff425c29f40c4772b3
goatfishplays <goatfishplays@gmail.com>
2025-06-04 13:43:24 -0700

    base scene prefab also I broke audio probably

--- Assets/_Scripts/Audio/AudioPlayer.cs
+++ Assets/_Scripts/Audio/AudioPlayer.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 public class AudioPlayer : MonoBehaviour
 {
     public AudioSource audioSource;
+    public float pitchMin = 1f;
+    public float pitchMax = 1f;
+    private float pitch = 1f;
     public Transform tracker;
     public Coroutine co_audioPlaying = null;
 
@@ -15,25 +18,53 @@ public class AudioPlayer : MonoBehaviour
         }
     }
 
-    public void PlayAudioAtTracker(AudioClip clip, Transform tracker, float volume = 1f, float pitch = 1f, bool loop = false)
+    #region Pitch
+    public float GetPitch()
+    {
+        return pitch;
+    }
+
+    public void SetPitch(float value)
+    {
+        pitch = value;
+    }
+
+    public void RandomizePitch()
+    {
+        pitch = Random.Range(pitchMin, pitchMax);
+        audioSource.pitch = pitch;
+    }
+
+    public void RandomizePitch(float pitchMin, float pitchMax)
+    {
+        this.pitchMin = pitchMin;
+        this.pitchMax = pitchMax;
+        RandomizePitch();
+    }
+    #endregion Pitch
+
+
+    #region Playing
+    public void PlayAudioAtTracker(AudioClip clip, Transform tracker, float volume = 1f, float pitchMin = 1f, float pitchMax = 1f, bool loop = false)
     {
         this.tracker = tracker;
         transform.SetPositionAndRotation(tracker.position, tracker.rotation);
-        PlayAudio(clip, volume, pitch, loop);
+        PlayAudio(clip, volume, pitchMin, pitchMax, loop);
     }
 
-    public void PlayAudioAtPoint(AudioClip clip, Vector3 pos, Quaternion rot, float volume = 1f, float pitch = 1f, bool loop = false)
+    public void PlayAudioAtPoint(AudioClip clip, Vector3 pos, Quaternion rot, float volume = 1f, float pitchMin = 1f, float pitchMax = 1f, bool loop = false)
     {
         transform.position = pos;
         transform.rotation = rot;
-        PlayAudio(clip, volume, pitch, loop);
+        PlayAudio(clip, volume, pitchMin, pitchMax, loop);
     }
 
-    public void PlayAudio(AudioClip clip, float volume = 1f, float pitch = 1f, bool loop = false)
+    public void PlayAudio(AudioClip clip, float volume = 1f, float pitchMin = 1f, float pitchMax = 1f, bool loop = false)
     {
+
         audioSource.clip = clip;
         audioSource.volume = volume;
-        audioSource.pitch = pitch;
+        RandomizePitch(pitchMin, pitchMax);
         audioSource.loop = loop;
 
         if (co_audioPlaying != null)
@@ -42,12 +73,33 @@ public class AudioPlayer : MonoBehaviour
         }
 
         audioSource.Play();
-        if (!loop)
-        {
-            co_audioPlaying = StartCoroutine(Playing(clip.length));
-        }
+        // if (!loop)
+        // {
+        co_audioPlaying = StartCoroutine(Playing(clip.length));
+        // }
     }
 
+    private IEnumerator Playing(float time)
+    {
+        // yield return new WaitForSeconds(time + .1f);
+        do
+        {
+            yield return new WaitForSeconds(time);
+            RandomizePitch();
+        } while (audioSource.loop);
+        // reset and return 
+        FreeSelf();
+    }
+
+    #endregion Playing
+
+    public void FreeSelf()
+    {
+        audioSource.Stop();
+        AudioManager.instance.FreeSFXPlayer(this);
+        tracker = null;
+        transform.parent = AudioManager.instance.transform;
+    }
 
     public void Pause()
     {
@@ -67,14 +119,6 @@ public class AudioPlayer : MonoBehaviour
         }
     }
 
-    private IEnumerator Playing(float time)
-    {
-        yield return new WaitForSeconds(time + .1f);
-        // reset and return 
-        AudioManager.instance.FreeSFXPlayer(this);
-        tracker = null;
-        transform.parent = AudioManager.instance.transform;
-    }
 
 
     private void OnDestroy()
