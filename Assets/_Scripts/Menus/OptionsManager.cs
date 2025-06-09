99a04f65f266b999fcf31b7f3c3352fcc7a4de59
goatfishplays <goatfishplays@gmail.com>
2025-06-09 10:19:57 -0700

    Added Sliders With Text Input Field Capability

--- Assets/_Scripts/Menus/OptionsManager.cs
+++ Assets/_Scripts/Menus/OptionsManager.cs
@@ -3,6 +3,34 @@ using UnityEngine;
 public class OptionsManager : MonoBehaviour
 {
     public static OptionsManager instance;
+
+    [SerializeField] private InputSliderSync xSensitivityHolder;
+    [SerializeField] private float _xSensitivity;
+    public float xSensitivity => _xSensitivity;
+    public void SetXSensitivity(float xSensitivity)
+    {
+        xSensitivityHolder.SetValue(xSensitivity);
+    }
+
+    public void FetchXSensitivity()
+    {
+        _xSensitivity = xSensitivityHolder.curVal;
+    }
+    [SerializeField] private InputSliderSync ySensitivityHolder;
+    [SerializeField] private float _ySensitivity;
+    public float ySensitivity => _ySensitivity;
+    public void SetYSensitivity(float ySensitivity)
+    {
+        ySensitivityHolder.SetValue(ySensitivity);
+    }
+
+    public void FetchYSensitivity()
+    {
+        _ySensitivity = ySensitivityHolder.curVal;
+    }
+
+
+
     private void Awake()
     {
         if (instance == null)
@@ -14,21 +42,8 @@ public class OptionsManager : MonoBehaviour
             Debug.LogWarning("Two OptionsManagers detected deleting second");
             Destroy(this);
         }
+
+        xSensitivityHolder.onValueChanged.AddListener(FetchXSensitivity);
+        ySensitivityHolder.onValueChanged.AddListener(FetchYSensitivity);
     }
-
-    [SerializeField] private float _xSensitivity = 1f;
-    public float xSensitivity => _xSensitivity;
-    public void SetXSensitivity(float xSensitivity)
-    {
-        _xSensitivity = xSensitivity;
-    }
-
-    [SerializeField] private float _ySensitivity = 1f;
-    public float ySensitivity => _ySensitivity;
-    public void SetYSensitivity(float ySensitivity)
-    {
-        _ySensitivity = ySensitivity;
-    }
-
-
 }
