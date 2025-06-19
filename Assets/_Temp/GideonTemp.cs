87cec84685db05afff69e1afcbc4f88650b98848
Zippybyte <62920339+Zippybyte@users.noreply.github.com>
2025-06-18 23:03:05 -0700

    Remove unused reset scene from test

--- Assets/_Temp/GideonTemp.cs
+++ Assets/_Temp/GideonTemp.cs
@@ -5,19 +5,11 @@ using UnityEngine.UI;
 
 public class Gideon_Temp : MonoBehaviour
 {
-    public Button reset;
     public GameObject UI;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        reset.onClick.AddListener(ResetScene);
         UI.SetActive(true); // Show the UI again. I don't want to see it in the editor.
-    }
-
-    private void ResetScene()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-
     }
 }
