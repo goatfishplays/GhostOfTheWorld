48b896b5594a447a23960034c9a8972d85375212
Zippybyte <62920339+Zippybyte@users.noreply.github.com>
2025-06-18 22:58:27 -0700

    Make SceneSwitcher switch to active scene if string is empty

--- Assets/_Scripts/SceneSwitcher.cs
+++ Assets/_Scripts/SceneSwitcher.cs
@@ -4,8 +4,17 @@ using UnityEngine.SceneManagement;
 public class SceneSwitcher : MonoBehaviour
 {
 
+    // Loads the given sceneName. If sceneName is empty loads the active scene.
     public static void SwitchScene(string sceneName)
     {
+        if (sceneName == "")
+        {
+            // Loads the current scene again.
+            // This might be bad code practice idk
+            sceneName = SceneManager.GetActiveScene().name;
+            Debug.LogWarning("sceneName is empty, reloading current scene " + sceneName);
+        }
+
         SceneManager.LoadScene(sceneName);
     }
 
